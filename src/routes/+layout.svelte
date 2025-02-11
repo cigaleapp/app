67d6ded3946e9aee5402a4fa5bfc82aa22686ca3
Gwen Le Bihan <hey@ewen.works>
2025-02-11 18:59:09 +0100

    🐛 Fix base paths

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,18 @@
 <script>
+	import { base } from '$app/paths';
+
 	const { children } = $props();
 </script>
 
+<svelte:head>
+	<base href={base} />
+</svelte:head>
+
 {@render children?.()}
 
-
 <style>
-    :root {
-        --corner-radius: 7px;
-        --border-thickness: 1px
-    }
+	:root {
+		--corner-radius: 7px;
+		--border-thickness: 1px;
+	}
 </style>
--- src/routes/_components/+page.svelte
+++ src/routes/_components/+page.svelte
@@ -1,15 +1,9 @@
 <script>
-	import { goto } from '$app/navigation';
-
 	let componentName = $state('');
 </script>
 
 <input type="text" bind:value={componentName} />
 
-<button
-	onclick={() => {
-		goto(`/_components/${componentName}`);
-	}}
->
+<a href="/_components/{componentName}">
 	Aller à /_components/{componentName}
-</button>
+</a>
