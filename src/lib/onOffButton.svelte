0240328387b7632d8a4cbc9ed7f3d2eccf8630d0
Lamothe <lamotho>
2025-02-14 15:07:35 +0100

    💄 Using the right colors and icons
    closes #17 normalement

--- src/lib/onOffButton.svelte
+++ src/lib/onOffButton.svelte
@@ -32,7 +32,7 @@
 		height: 1.6em;
 		position: relative;
 		margin: 0 0 0 0.5em;
-		background: #2c004b;
+		background: var(--fg-primary);
 		border: none;
 		border-radius: 1.5em;
 	}
@@ -42,7 +42,7 @@
 		position: absolute;
 		width: 1.3em;
 		height: 1.3em;
-		background: white;
+		background: var(--bg-neutral);
 		top: 0.13em;
 		right: 1.5em;
 		transition: transform 0.3s;
@@ -50,7 +50,7 @@
 	}
 
 	button[aria-checked='true'] {
-		background-color: #25c8bf;
+		background-color: var(--bg-primary);
 	}
 
 	button[aria-checked='true']::before {
@@ -59,42 +59,42 @@
 	}
 
 	button:hover[aria-checked='true'] {
-		box-shadow: 0 0px 5px #25c8bf;
+		box-shadow: 0 0px 5px var(--bg-primary);
 		border-radius: 1.5em;
 	}
 
 	button:hover {
-		box-shadow: 0 0px 5px #2c004b;
+		box-shadow: 0 0px 5px var(--fg-primary);
 		border-radius: 1.5em;
 	}
 
 	button:focus[aria-checked='true'] {
-		box-shadow: 0 0px 8px #25c8bf;
+		box-shadow: 0 0px 8px var(--bg-primary);
 		border-radius: 1.5em;
 	}
 
 	button:focus {
-		box-shadow: 0 0px 8px #2c004b;
+		box-shadow: 0 0px 8px var(--fg-primary);
 		border-radius: 1.5em;
 	}
 
 	.s--dayNight::before {
-		background-image: url('./moon-symbol-512.webp');
+		background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNTYiIGhlaWdodD0iMjU2IiB2aWV3Qm94PSIwIDAgMjU2IDI1NiI+PHBhdGggZmlsbD0iY3VycmVudENvbG9yIiBkPSJNMjMyLjEzIDE0My42NGE2IDYgMCAwIDAtNi0xLjQ5YTkwLjA3IDkwLjA3IDAgMCAxLTExMi4yNy0xMTIuM2E2IDYgMCAwIDAtNy40OS03LjQ4YTEwMi44OCAxMDIuODggMCAwIDAtNTEuODkgMzYuMzFhMTAyIDEwMiAwIDAgMCAxNDIuODQgMTQyLjg0YTEwMi44OCAxMDIuODggMCAwIDAgMzYuMzEtNTEuODlhNiA2IDAgMCAwLTEuNS01Ljk5bS00MiA0OC4yOWE5MCA5MCAwIDAgMS0xMjYtMTI2YTkwLjkgOTAuOSAwIDAgMSAzNS41Mi0yOC4yN2ExMDIuMDYgMTAyLjA2IDAgMCAwIDExOC42OSAxMTguNjlhOTAuOSA5MC45IDAgMCAxLTI4LjI0IDM1LjU4WiIvPjwvc3ZnPg==');
 		background-size: cover;
 	}
 
 	.s--dayNight[aria-checked='true']::before {
-		background-image: url('./sun-symbol-512.webp');
+		background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNTYiIGhlaWdodD0iMjU2IiB2aWV3Qm94PSIwIDAgMjU2IDI1NiI+PHBhdGggZmlsbD0iY3VycmVudENvbG9yIiBkPSJNMTIyIDQwdi04YTYgNiAwIDAgMSAxMiAwdjhhNiA2IDAgMCAxLTEyIDBtNjggODhhNjIgNjIgMCAxIDEtNjItNjJhNjIuMDcgNjIuMDcgMCAwIDEgNjIgNjJtLTEyIDBhNTAgNTAgMCAxIDAtNTAgNTBhNTAuMDYgNTAuMDYgMCAwIDAgNTAtNTBNNTkuNzYgNjguMjRhNiA2IDAgMSAwIDguNDgtOC40OGwtOC04YTYgNiAwIDAgMC04LjQ4IDguNDhabTAgMTE5LjUybC04IDhhNiA2IDAgMSAwIDguNDggOC40OGw4LThhNiA2IDAgMSAwLTguNDgtOC40OG0xMzYtMTM2bC04IDhhNiA2IDAgMSAwIDguNDggOC40OGw4LThhNiA2IDAgMCAwLTguNDgtOC40OG0uNDggMTM2YTYgNiAwIDAgMC04LjQ4IDguNDhsOCA4YTYgNiAwIDAgMCA4LjQ4LTguNDhaTTQwIDEyMmgtOGE2IDYgMCAwIDAgMCAxMmg4YTYgNiAwIDAgMCAwLTEybTg4IDg4YTYgNiAwIDAgMC02IDZ2OGE2IDYgMCAwIDAgMTIgMHYtOGE2IDYgMCAwIDAtNi02bTk2LTg4aC04YTYgNiAwIDAgMCAwIDEyaDhhNiA2IDAgMCAwIDAtMTIiLz48L3N2Zz4=');
 		background-size: cover;
 	}
 
 	.s--crossCheck::before {
-		background-image: url('./cross-23.png');
+		background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNTYiIGhlaWdodD0iMjU2IiB2aWV3Qm94PSIwIDAgMjU2IDI1NiI+PHBhdGggZmlsbD0iY3VycmVudENvbG9yIiBkPSJNMjAyLjgzIDE5Ny4xN2E0IDQgMCAwIDEtNS42NiA1LjY2TDEyOCAxMzMuNjZsLTY5LjE3IDY5LjE3YTQgNCAwIDAgMS01LjY2LTUuNjZMMTIyLjM0IDEyOEw1My4xNyA1OC44M2E0IDQgMCAwIDEgNS42Ni01LjY2TDEyOCAxMjIuMzRsNjkuMTctNjkuMTdhNCA0IDAgMSAxIDUuNjYgNS42NkwxMzMuNjYgMTI4WiIvPjwvc3ZnPg==');
 		background-size: cover;
 	}
 
 	.s--crossCheck[aria-checked='true']::before {
-		background-image: url('./OOjs_UI_icon_check.png');
+		background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNTYiIGhlaWdodD0iMjU2IiB2aWV3Qm94PSIwIDAgMjU2IDI1NiI+PHBhdGggZmlsbD0iY3VycmVudENvbG9yIiBkPSJtMjI4LjI0IDc2LjI0bC0xMjggMTI4YTYgNiAwIDAgMS04LjQ4IDBsLTU2LTU2YTYgNiAwIDAgMSA4LjQ4LTguNDhMOTYgMTkxLjUxTDIxOS43NiA2Ny43NmE2IDYgMCAwIDEgOC40OCA4LjQ4Ii8+PC9zdmc+');
 		background-size: cover;
 	}
 </style>
