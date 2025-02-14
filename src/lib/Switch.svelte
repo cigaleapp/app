8ee711bd01996cf5e764d1b66fe6dc46fa760ad2
Lamothe <lamotho>
2025-02-14 17:03:39 +0100

    💄 Its beautiful switch now
    closes #17FINALLY

---
+++ src/lib/Switch.svelte
@@ -0,0 +1,87 @@
+<script>
+	import { getContext } from 'svelte';
+	import Check from '~icons/ph/check-bold';
+	import Cross from '~icons/ph/x-bold';
+
+	let { pushed = $bindable(), icons: iconsOverride = undefined } = $props();
+
+	function click() {
+		pushed = !pushed;
+	}
+
+	/** @type {boolean} */
+	let showHints = getContext('showSwitchHints');
+
+	let icons = $derived(
+		iconsOverride ?? {
+			on: Check,
+			off: Cross
+		}
+	);
+</script>
+
+<button
+	class="switch"
+	role="switch"
+	aria-checked={pushed}
+	onclick={click}
+	aria-labelledby="On off switch"
+>
+	<div class="handle" class:pushed>
+		{#if showHints || iconsOverride}
+			{#if pushed}
+				<icons.on />
+			{:else}
+				<icons.off />
+			{/if}
+		{/if}
+	</div>
+</button>
+
+<style>
+	.switch {
+		cursor: pointer;
+		--handle-size: 1.5rem;
+		--pad: 0.25rem;
+		display: flex;
+		align-items: center;
+		width: calc(2 * var(--handle-size) + 2 * var(--pad));
+		height: calc(var(--handle-size) + 2 * var(--pad));
+		background: var(--bg-neutral);
+		border: 1px var(--fg-dark-primary) solid;
+		border-radius: 1.5em;
+	}
+
+	.handle {
+		font-size: 0.8em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		content: '';
+		width: var(--handle-size);
+		height: var(--handle-size);
+		background: var(--switch-bg, var(--fg-neutral));
+		border-radius: 100%;
+		transition: margin 0.25s ease;
+		color: var(--bg-neutral);
+	}
+
+	.handle.pushed {
+		margin-left: var(--handle-size);
+		background-color: var(--bg-primary);
+		color: var(--fg-primary);
+	}
+
+	.switch:is(:hover, :focus-visible) {
+		border-color: var(--bg-primary);
+		background-color: var(--bg-primary-translucent);
+	}
+
+	.switch:is(:hover, :focus-visible) .handle {
+		color: var(--bg-primary);
+	}
+
+	.switch:is(:hover, :focus-visible) .handle.pushed {
+		color: var(--fg-neutral);
+	}
+</style>
