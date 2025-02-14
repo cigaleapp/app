e40e792c31f3a88b39ecd70a155e45868a4bfae2
WhiSp00n <gaetanlaumonier493@gmail.com>
2025-02-14 15:27:23 +0100

    🍱 Ça prends forme (la roue elle tourne OMG)
    Voila voila Gwenn need help UwU

---
+++ src/lib/SettingWheel.svelte
@@ -0,0 +1,57 @@
+<script>
+	import Icon from '@iconify/svelte';
+	let isActive = false;
+	let isDivActive = false;
+
+	function handleClick() {
+		isActive = !isActive;
+		isDivActive = !isDivActive;
+	}
+</script>
+
+<div class:isDivActive class="container" aria-pressed={isDivActive}>
+	<button class:isActive onclick={handleClick} aria-pressed={isActive} type="button">
+		<Icon class="icon" icon="ph:gear-light" />
+	</button>
+</div>
+
+<style>
+	:root {
+		font-size: em;
+		color: var(--fg-primary);
+	}
+
+	.isDivActive {
+		background-color: var(--bg-primary);
+	}
+
+	button {
+		position: fixed;
+		display: flex;
+		color: var(--color);
+		background: none;
+		border: none;
+		cursor: pointer;
+	}
+
+	button:hover {
+		color: #35c8bf;
+	}
+
+	.isActive {
+		position: fixed;
+		display: flex;
+		color: red;
+		transform: rotate(360deg);
+		transition: transform 1s ease-in-out;
+	}
+
+	.isDivActive {
+		position: fixed;
+		display: flex;
+		height: 1vh;
+		width: 1vw;
+		background-color: var(--bg-primary);
+		transition: background-color 1s ease-in-out;
+	}
+</style>
